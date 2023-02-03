4e936fe8820ccaebe1f0b5e8a1240611a07c1ded
YanSardinha <yansardinha1001@gmail.com>
2023-02-03 10:55:05 -0300

    Updating to deploy

--- StockControl/settings.py
+++ StockControl/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://stockcontrol-develop.up.railway.app, https://web-production-2b51.up.railway.app']
 
 CSRF_COOKIE_DOMAIN = 'stockcontrol-develop.up.railway.app'
 
