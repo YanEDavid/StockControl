8a8f7e3c18e1aa18aebdc6b5f9e93b870d019184
YanSardinha <yansardinha1001@gmail.com>
2023-02-06 09:06:26 -0300

    Atualizando templates de login

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from users import views
 urlpatterns = [
     path('usuario/cadastro/', registerPage, name="cadastro"),
     path('usuario/list/', views.ListaUsuario.as_view(), name="list_users"),
-    path('usuario/login/', include("django.contrib.auth.urls")),
+    path('usuario/', include("django.contrib.auth.urls"), name="login"),
 ]
